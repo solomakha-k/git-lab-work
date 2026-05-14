36f49fd0952fc5c8425b7312307909aef5fb0f29
solomakha-k <solomakha.k@donnu.edu.ua>
2026-05-14 20:21:39 +0000

    feat: додаємо структуру проекту та main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+def greet(name: str) -> str:
+    """Повертає вітальне повідомлення."""
+    return f"Привіт, {name}! "
+
+if __name__=="__main__ ":
+    print(greet("Студент "))
